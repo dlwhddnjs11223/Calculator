32e972f2ecd21c8e839d8cb304276c2ab096439c
ejong_1@naver.com <ejong_1@naver.com>
2024-04-28 17:02:34 +0900

    과제 요구사항 2 완료, 30분 소요

--- App.java
+++ App.java
@@ -1,44 +1,41 @@
 package Work;
 
+import java.util.Objects;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {//양의 정수 입력 받기
-
+int Number1;
+int Number2;
+char Operation;
 
         Scanner sc = new Scanner(System.in);
 
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
 
-            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-            try {
-                String stringNumber1 = sc.nextLine(); // String type으로 숫자를 입력받고
-                double doubleNumber1 = Double.parseDouble(stringNumber1); // 입력받은 숫자를 double 타입으로 변환한다.
-                if (doubleNumber1 < 0 || doubleNumber1 % 1 != 0) {  // 만약 double 타입으로 변환된 숫자가 0보다 작거나, 양수가 아닐 때
-                    System.out.println("양의 정수만 입력해주세요");    // 양의 정수만 입력해달라는 알람을 띄운다.
-                } else {                                            // 그 외의 경우에
-                int inNumber1 = (int) doubleNumber1;}                // int number1이라는 변수에 int형으로 변한된 doubleNumber1 값을 저장한다.
-            } catch (NumberFormatException e) {                     // 예외가 발생할 경우에
-                System.out.println("양의 정수만 입력해주세요");        // 양의 정수만 입력해달라는 알람을 띄운다.
+            int intNumber1 = sc.nextInt();
+            if (intNumber1 < 0) {
+                System.out.println("양의 정수만 입력해주세요");
+            } else {
+                Number1 = intNumber1;
             }
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+            Operation = sc.next().charAt(0);
+
 
             System.out.print("두 번째 숫자를 입력하세요: ");
 
-            // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
-            try {
-                String stringNumber2 = sc.nextLine(); // String type으로 숫자를 입력받고
-                double doubleNumber2 = Double.parseDouble(stringNumber2);
-                if (doubleNumber2 < 0 || doubleNumber2 % 1 != 0) {
-                    System.out.println("양의 정수만 입력해주세요");
-                }
-                int inNumber2 = (int) doubleNumber2;
-            } catch (NumberFormatException e) {
+            int intNumber2 = sc.nextInt();
+            if (intNumber2 < 0) {
                 System.out.println("양의 정수만 입력해주세요");
+            } else {
+                Number2 = intNumber2;
             }
 
-
-//                    System.out.println(intNumber1+" + "+intNumber2);
+            System.out.println(Number1+Number2);
         }
     }
 }
