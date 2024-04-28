3bdc659d271b25ae1eadd4b00418c517e58d0ff8
ejong_1@naver.com <ejong_1@naver.com>
2024-04-28 19:24:01 +0900

    과제 요구사항 3 수정

--- App.java
+++ App.java
@@ -5,48 +5,53 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {//양의 정수 입력 받기
-        int Number1 = 0;
-        int Number2 = 0;
-        char Operation = 'a';
+
+
 
 
         Scanner sc = new Scanner(System.in);
+
 
         while (true) {
             System.out.print("첫 번째 숫자를 입력하세요: ");
 
             int intNumber1 = sc.nextInt();
-            if (intNumber1 > 0) {
-                Number1 = intNumber1;
-            }
+
+            double Number1 = intNumber1;
+
 
             System.out.print("사칙연산 기호를 입력하세요: ");
             // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
             char charOperation = sc.next().charAt(0);
-            if (charOperation == '+' || charOperation == '-' || charOperation == '*' || charOperation == '/') {
-                Operation = charOperation;
-            }
+
+            char Operation = charOperation;
 
 
             System.out.print("두 번째 숫자를 입력하세요: ");
 
             int intNumber2 = sc.nextInt();
-            if (intNumber2 > 0) {
-                Number2 = intNumber2;
-            }
+
+            double Number2 = intNumber2;
 
 
+            double result = 0;
             if (Operation == '+') {
-                System.out.println(Number1 + Number2);
+                result = Number1 + Number2;
             } else if (Operation == '-') {
-                System.out.println(Number1 - Number2);
+                result = Number1 - Number2;
             } else if (Operation == '*') {
-                System.out.println(Number1 * Number2);
+                result = Number1 * Number2;
             } else if (Operation == '/') {
-                System.out.println(Number1 / Number2);
+                result = Number1 / Number2;
             }
 
-
+            if(Operation == '/' && Number1 <= 0) {
+                System.out.println("나눗셈 연산에서 분자(첫번째 정수)에 0이 입력될 수 없습니다.");
+            } else if (Operation == '/' && Number2 <= 0) {
+                System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+            } else {
+                System.out.println("결과 : " + result);
+            }
         }
     }
 }
